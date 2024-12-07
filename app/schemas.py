322d16987e415eb9dcb6dd6673ee6b01f1ace2d1
Pedro Pina <pedropinacolider@gmail.com>
2024-12-06 21:58:03 -0400

    criado modelos para paginação

--- app/schemas.py
+++ app/schemas.py
@@ -4,3 +4,32 @@ from typing import TypeVar, Generic
 T = TypeVar("T")
 
 
+class BasePaginacao(BaseModel):
+    """
+    Modelo base para rotas
+    """
+
+    pagina: int
+    limite: int
+
+
+class PaginacaoResponse(BasePaginacao, Generic[T]):
+    """
+    Modelo de paginacao utilizado para retornos de rotas paginadas
+    """
+    
+    conteudo: list[T]
+    qtd_itens: int
+    qtd_paginas: int
+
+
+class PaginacaoRequest(BasePaginacao, Generic[T]):
+    filtro: T
+
+
+class FiltroMedico(BaseModel):
+
+    nome: str
+    id: str
+    conselho: str
+    uf_conselho: str
