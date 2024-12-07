ded87f1ff0d7564891daf2271fb639979705909e
Pedro Pina <pedropinacolider@gmail.com>
2024-12-07 10:16:45 -0400

    ajustado e criado tipo de dependecia de sessão para reduzir repeticao de codigo

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,3 +1,6 @@
+from typing import Annotated
+from fastapi import Depends
+
 from app.infra.db import engine, Session
 
 
@@ -5,3 +8,5 @@ from app.infra.db import engine, Session
 def get_session():
     with Session(engine) as session:
         yield session
+
+SessionDeps = Annotated[Session, Depends(get_session)]
--- app/routes/medico.py
+++ app/routes/medico.py
@@ -2,29 +2,26 @@ from fastapi import APIRouter, Body, Depends
 from typing import Annotated
 from sqlmodel import Session
 
-from app.dependencies import get_session
+from app.dependencies import SessionDeps
 from app import crud, models, schemas
 
 
 medico_router = APIRouter(prefix='/medico', tags=['Medico'])
 
-@medico_router.post('/')
-def encontra_medico(
-    session: Session = Depends(get_session)
-                    )-> list[models.Medico]:
-    """
-    Realize a consulta de médicos cadastrados na plataforma
-    """
-    medicos = crud.procura_medicos(session)    
 
-    return medicos
 
 @medico_router.post('/search')
 def encontra_medico_com_paginacao(
     body: Annotated[schemas.PaginacaoRequest[schemas.FiltroMedico], Body()],
-    session: Session = Depends(get_session),
+    session: SessionDeps,
                             ) -> schemas.PaginacaoResponse[models.Medico]:
 
     retorno = crud.procura_medicos_paginado(session, body.pagina, body.limite, body.filtro)
     
     return retorno
+
+
+@medico_router.post('/')
+def inclui_novo_medico(body: Annotated[models.Medico, Body()],
+                       session: SessionDeps):
+    ...
