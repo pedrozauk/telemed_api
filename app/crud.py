fe093e0ce5c8b21f9b9d54d28b5b47c04e072ddc
Pedro Pina <pedropinacolider@gmail.com>
2024-12-07 10:11:21 -0400

    criado consulta paginada medicos

--- app/crud.py
+++ app/crud.py
@@ -1,8 +1,80 @@
 from sqlmodel import select, Session
-from app import models
+from sqlalchemy import func
+from math import ceil
+
+from app import models, schemas
 
 
 
-def procura_medicos(session: Session)->list[models.Medico]:
+def procura_medicos(session: Session,
+                    filtro: schemas.FiltroMedico|None = None,
+
+                    )->list[models.Medico]:
+    """
+    Função para consulta de médicos no banco de dados
+    """
+
+    # Inicializa stmt para realizar os filtros e paginaççao posterior 
+    # caso necessário
+
     stmt = select(models.Medico)
+
+    if filtro:
+        if filtro.conselho:
+            stmt = stmt.where(models.Medico.conselho == filtro.conselho)
+
+        if filtro.uf_conselho:
+            stmt = stmt.where(models.Medico.uf_conselho == filtro.uf_conselho)
+
+        if filtro.nome:
+            stmt = stmt.where(models.Medico.nome_completo == filtro.nome)
+
     return session.exec(stmt).all()
+
+
+def procura_medicos_paginado(session: Session,
+                    pagina: int,
+                    limite: int,
+                    filtro: schemas.FiltroMedico|None = None,
+
+                    )->schemas.PaginacaoResponse[list[models.Medico]]:
+    """
+    Função para consulta de médicos no banco de dados
+    """
+    
+    offset = (pagina - 1) * limite
+
+    # Inicializa stmt para realizar os filtros e paginaççao posterior 
+    # caso necessário
+
+    stmt = select(models.Medico)
+
+
+
+    if filtro:
+        if filtro.conselho:
+            stmt = stmt.where(models.Medico.conselho == filtro.conselho)
+
+        if filtro.uf_conselho:
+            stmt = stmt.where(models.Medico.uf_conselho == filtro.uf_conselho)
+
+        if filtro.nome:
+            stmt = stmt.where(models.Medico.nome_completo == filtro.nome)
+
+    total = session.exec(select(func.count()).select_from(stmt.subquery())).one()
+
+    paginas = 0
+    
+    if total:
+        paginas = ceil(total/limite)
+
+    medicos = session.exec(stmt).all()
+
+    return schemas.PaginacaoResponse[models.Medico](
+        pagina=pagina,
+        limite=limite,
+        qtd_itens=total,
+        qtd_paginas=paginas,
+        conteudo=medicos
+    )
+
--- app/routes/medico.py
+++ app/routes/medico.py
@@ -3,11 +3,12 @@ from typing import Annotated
 from sqlmodel import Session
 
 from app.dependencies import get_session
-from app import crud, models
+from app import crud, models, schemas
+
 
 medico_router = APIRouter(prefix='/medico', tags=['Medico'])
 
-@medico_router.post('/search')
+@medico_router.post('/')
 def encontra_medico(
     session: Session = Depends(get_session)
                     )-> list[models.Medico]:
@@ -18,3 +19,12 @@ def encontra_medico(
 
     return medicos
 
+@medico_router.post('/search')
+def encontra_medico_com_paginacao(
+    body: Annotated[schemas.PaginacaoRequest[schemas.FiltroMedico], Body()],
+    session: Session = Depends(get_session),
+                            ) -> schemas.PaginacaoResponse[models.Medico]:
+
+    retorno = crud.procura_medicos_paginado(session, body.pagina, body.limite, body.filtro)
+    
+    return retorno
